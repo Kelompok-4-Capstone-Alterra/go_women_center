0b160f4e495c9d4a0129ed3ac6ea8bd65c72f51b
ThiccPan <ihilmiderian@gmail.com>
2023-05-27 00:20:18 +0700

    chore: add request struct for verify email feature

--- user/request.go
+++ user/request.go
@@ -1,8 +1,12 @@
 package user
 
 type RegisterUserDTO struct {
-	Name string `json:"name"`
-	Email string `json:"email"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type VerifyEmailDTO struct {
+	Email string `json:"email"`
 }
