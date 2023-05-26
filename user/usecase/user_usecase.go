7ff4de9d2ff47a4f3ded35f38be87af2f5f779d8
ThiccPan <ihilmiderian@gmail.com>
2023-05-27 00:44:29 +0700

    fix: generate id before saving to repo

--- user/usecase/user_usecase.go
+++ user/usecase/user_usecase.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/domain"
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/helper"
 	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/user"
 	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/user/repository"
 )
@@ -12,20 +13,29 @@ type UserUsecase interface {
 
 type userUsecase struct {
 	repo repository.UserRepo
+	UuidGenerator helper.UuidGenerator
 }
 
-func NewUserUsecase(repo repository.UserRepo) *userUsecase {
+func NewUserUsecase(repo repository.UserRepo, idGenerator helper.UuidGenerator) *userUsecase {
 	return &userUsecase{
 		repo: repo,
+		UuidGenerator: idGenerator,
 	}
 }
 
 func (u *userUsecase) Register(userDTO user.RegisterUserDTO) (domain.User, error) {
+	uuid, err := u.UuidGenerator.GenerateUUID()
+	if err != nil {
+		return domain.User{}, err
+	}
+
 	data := domain.User{
+		ID: uuid,
 		Name: userDTO.Name,
 		Email: userDTO.Email,
 		Username: userDTO.Username,
 		Password: userDTO.Password,
 	}
+
 	return u.repo.Create(data)
 }
