157ea502df6db27a0b0976d179b0569a041f64bf
ThiccPan <ihilmiderian@gmail.com>
2023-05-27 00:17:54 +0700

    feature: otp code generator

---
+++ helper/otp_generator.go
@@ -0,0 +1,28 @@
+package helper
+
+import (
+	"crypto/rand"
+	"io"
+)
+
+func GetOtp() (string, error) {
+	code := make([]byte, OTP_SIZE)
+
+	n, err := io.ReadAtLeast(rand.Reader, code, OTP_SIZE)
+	if n != OTP_SIZE {
+		return "", err
+	}
+
+	for i := 0; i < len(code); i++ {
+		// iterate each code byte val after randomizing,
+		// then change the value by modulo the val by len of num list
+		// then we get random index of table value
+		code[i] = table[int(code[i])%len(table)]
+	}
+
+	return string(code), nil
+}
+
+const OTP_SIZE = 4
+
+var table = [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
