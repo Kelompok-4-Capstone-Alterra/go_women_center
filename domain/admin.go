dd078b11c337b1a62a6b9c04220e91517f2c1a58
ThiccPan <ihilmiderian@gmail.com>
2023-05-29 04:04:40 +0700

    fix: remove tag

--- domain/admin.go
+++ domain/admin.go
@@ -1,8 +1,8 @@
 package domain
 
 type Admin struct {
-	Id       string `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Username string `json:"username"`
+	Id       string `gorm:"primaryKey"`
+	Email    string
+	Password string
+	Username string
 }
