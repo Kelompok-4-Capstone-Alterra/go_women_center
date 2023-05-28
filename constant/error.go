acdd0e81de6e002268fd07a2fe076a56bad7bcc2
ThiccPan <ihilmiderian@gmail.com>
2023-05-28 13:16:13 +0700

    feature: add new error

--- constant/error.go
+++ constant/error.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrUserNotInCache = errors.New("the user isn't in cache")
 	ErrExpiredCache = errors.New("the otp is already expired")
+	ErrInvalidCredential = errors.New("invalid credential")
 )
