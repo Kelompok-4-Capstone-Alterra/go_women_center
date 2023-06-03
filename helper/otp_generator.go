e0a85afc093b6f4aa0c495872220c384bebb430b
ThiccPan <ihilmiderian@gmail.com>
2023-06-03 21:58:37 +0700

    fix: otp generator not sending code

--- helper/otp_generator.go
+++ helper/otp_generator.go
@@ -9,23 +9,17 @@ type OtpGenerator interface {
 	GetOtp() (string, error)
 }
 
-type otpGenerator struct {
-	OTP_SIZE int
-	table []byte
-}
+type otpGenerator struct {}
 
-func NewOtpGenerator(size int, table []byte) *otpGenerator {
-	return &otpGenerator{
-		OTP_SIZE: size,
-		table: table,
-	}
+func NewOtpGenerator() *otpGenerator {
+	return &otpGenerator{}
 }
 
 func (o *otpGenerator) GetOtp() (string, error) {
-	code := make([]byte, o.OTP_SIZE)
+	code := make([]byte, OTP_SIZE)
 
-	n, err := io.ReadAtLeast(rand.Reader, code, o.OTP_SIZE)
-	if n != o.OTP_SIZE {
+	n, err := io.ReadAtLeast(rand.Reader, code, OTP_SIZE)
+	if n != OTP_SIZE || err != nil {
 		return "", err
 	}
 
@@ -33,8 +27,12 @@ func (o *otpGenerator) GetOtp() (string, error) {
 		// iterate each code byte val after randomizing,
 		// then change the value by modulo the val by len of num list
 		// then we get random index of table value
-		code[i] = o.table[int(code[i])%len(o.table)]
+		code[i] = table[int(code[i])%len(table)]
 	}
 
 	return string(code), nil
 }
+
+const OTP_SIZE = 4
+
+var table = [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
