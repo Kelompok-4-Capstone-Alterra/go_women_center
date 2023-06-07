1cfc239a754349c9c0238728ddb7aed8e8c6ddd9
Ganes556 <gustiganes@gmail.com>
2023-06-07 19:39:25 +0800

    chore: enable ssl

--- app/main.go
+++ app/main.go
@@ -163,7 +163,7 @@ func main() {
 
 
 	// ssl
-	// e.Logger.Fatal(e.StartTLS(":8080", "./ssl/certificate.crt", "./ssl/private.key"))
+	e.Logger.Fatal(e.StartTLS(":8080", "./ssl/certificate.crt", "./ssl/private.key"))
 
-	e.Logger.Fatal(e.Start(":8080"))
+	// e.Logger.Fatal(e.Start(":8080"))
 }
