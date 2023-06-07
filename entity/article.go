c3090b450de8458251e2dbea54a1774f1858a05e
Saident <averilprimayuda@gmail.com>
2023-06-08 01:33:57 +0700

    Entity Article

--- entity/article.go
+++ entity/article.go
@@ -8,15 +8,21 @@ import (
 
 type Article struct {
 	ID           string `gorm:"primary_key;type:varchar(36);uniqueindex;not null"`
-	Title        string `gorm:"type:varchar(150);not null"`
 	Image        string `gorm:"type:varchar(255)"`
+	Title        string `gorm:"type:varchar(150);not null"`
 	Author       string `gorm:"type:varchar(150);not null"`
 	Topic        string `gorm:"type:varchar(50)"`
+	ViewCount    int    `gorm:"type:int;default:0"`
+	CommentCount int    `gorm:"type:int;default:0"`
 	Description  string
-	ViewCount    int       `gorm:"type:int"`
-	CommentCount int       `gorm:"type:int"`
+	Comments     []Comment `gorm:"foreignkey:ArticleID"`
 	Date         time.Time `gorm:"type:date"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 	DeletedAt    gorm.DeletedAt `gorm:"index"`
+}
+
+func (c *Article) BeforeDeleteArticle(tx *gorm.DB) error {
+	tx.Model(&Comment{}).Where("article_id = ?", c.ID).Delete(&Comment{})
+	return nil
 }
