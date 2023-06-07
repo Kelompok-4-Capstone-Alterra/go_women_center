7116225d8a41c83c6625f226beb28bc73465725b
Saident <averilprimayuda@gmail.com>
2023-06-08 01:34:04 +0700

    Entity Comment

--- entity/comment.go
+++ entity/comment.go
@@ -1,1 +1,17 @@
 package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Comment struct {
+	ID        string `gorm:"primary_key;type:varchar(36);uniqueindex;not null"`
+	ArticleID string `gorm:"type:varchar(36);not null"`
+	UserID    string `gorm:"type:varchar(36);not null"`
+	Comment   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
