afef7b65eb72bfff925daa6c128e5bec51e217cc
Saident <averilprimayuda@gmail.com>
2023-06-08 01:35:19 +0700

    admin article

--- admin/article/repository/gorm_mysql_article.go
+++ admin/article/repository/gorm_mysql_article.go
@@ -1,5 +1,95 @@
 package repository
 
-//CRUD article
-//CRD comment
-//Sort by most viewed, topic, newest, older
+import (
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/admin/article"
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/entity"
+	"gorm.io/gorm"
+)
+
+type ArticleRepository interface {
+	GetAll(search string, offset, limit int) ([]article.GetAllResponse, int64, error)
+	GetById(id string) (article.GetByResponse, error)
+	Create(article entity.Article) error
+	Update(id string, article entity.Article) error
+	Delete(id string) error
+	Count() (int, error)
+}
+
+type mysqlArticleRepository struct {
+	DB *gorm.DB
+}
+
+func NewMysqlArticleRepository(db *gorm.DB) ArticleRepository {
+	return &mysqlArticleRepository{DB: db}
+}
+
+func (r *mysqlArticleRepository) GetAll(search string, offset, limit int) ([]article.GetAllResponse, int64, error) {
+	var article []article.GetAllResponse
+	var count int64
+	err := r.DB.Model(&entity.Article{}).
+		Where("topic LIKE ? OR title LIKE ? OR author LIKE ?",
+			"%"+search+"%", "%"+search+"%", "%"+search+"%").
+		Count(&count).
+		Offset(offset).
+		Limit(limit).
+		Find(&article).Error
+
+	if err != nil {
+		return nil, 0, err
+	}
+	return article, count, nil
+}
+
+func (r *mysqlArticleRepository) GetById(id string) (article.GetByResponse, error) {
+	var article article.GetByResponse
+	err := r.DB.Model(&entity.Article{}).First(&article, "id = ?", id).Error
+	if err != nil {
+		return article, err
+	}
+
+	// article.ViewCount++
+
+	// err = r.DB.Model(&entity.Article{}).Where("id = ?", id).Updates(entity.Article{ViewCount: article.ViewCount}).Error
+	// if err != nil {
+	// 	return article, err
+	// }
+
+	return article, nil
+}
+
+func (r *mysqlArticleRepository) Create(article entity.Article) error {
+	err := r.DB.Create(&article).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *mysqlArticleRepository) Update(id string, article entity.Article) error {
+
+	err := r.DB.Model(&entity.Article{}).Where("id = ?", id).Updates(article).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *mysqlArticleRepository) Delete(id string) error {
+
+	err := r.DB.Delete(&entity.Article{}, "id = ?", id).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *mysqlArticleRepository) Count() (int, error) {
+	var count int64
+	err := r.DB.Model(&entity.Article{}).Count(&count).Error
+	if err != nil {
+		return 0, err
+	}
+	return int(count), nil
+}
--- admin/article/usecase/article_usecase.go
+++ admin/article/usecase/article_usecase.go
@@ -1,1 +1,148 @@
 package usecase
+
+import (
+	"mime/multipart"
+
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/admin/article"
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/admin/article/repository"
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/constant"
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/entity"
+
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/helper"
+)
+
+type ArticleUsecase interface {
+	GetAll(search string, offset, limit int) ([]article.GetAllResponse, int, error)
+	GetTotalPages(limit int) (int, error)
+	GetById(id string) (article.GetByResponse, error)
+	Create(inputDetail article.CreateRequest, inputImage *multipart.FileHeader) error
+	Update(inputDetail article.UpdateRequest, inputImage *multipart.FileHeader) error
+	Delete(id string) error
+}
+
+type articleUsecase struct {
+	articleRepo repository.ArticleRepository
+	image       helper.Image
+}
+
+func NewArticleUsecase(CRepo repository.ArticleRepository, Image helper.Image) ArticleUsecase {
+	return &articleUsecase{articleRepo: CRepo, image: Image}
+}
+
+func (u *articleUsecase) GetAll(search string, offset, limit int) ([]article.GetAllResponse, int, error) {
+
+	articles, totalData, err := u.articleRepo.GetAll(search, offset, limit)
+
+	if err != nil {
+		return nil, 0, article.ErrInternalServerError
+	}
+
+	return articles, helper.GetTotalPages(int(totalData), limit), nil
+}
+
+func (u *articleUsecase) GetTotalPages(limit int) (int, error) {
+
+	totalData, err := u.articleRepo.Count()
+	if err != nil {
+		return 0, article.ErrInternalServerError
+	}
+
+	totalPages := helper.GetTotalPages(totalData, limit)
+
+	return totalPages, nil
+}
+
+func (u *articleUsecase) GetById(id string) (article.GetByResponse, error) {
+
+	articleData, err := u.articleRepo.GetById(id)
+
+	if err != nil {
+		return articleData, article.ErrArticleNotFound
+	}
+
+	return articleData, nil
+}
+
+func (u *articleUsecase) Create(inputDetail article.CreateRequest, inputImage *multipart.FileHeader) error {
+	path, err := u.image.UploadImageToS3(inputImage)
+
+	if err != nil {
+		return article.ErrInternalServerError
+	}
+	uuid, _ := helper.NewGoogleUUID().GenerateUUID()
+
+	newArticle := entity.Article{
+		ID:          uuid,
+		Title:       inputDetail.Title,
+		Topic:       constant.TOPICS[inputDetail.Topic],
+		Author:      inputDetail.Author,
+		Description: inputDetail.Description,
+		Image:       path,
+	}
+
+	err = u.articleRepo.Create(newArticle)
+
+	if err != nil {
+		return article.ErrInternalServerError
+	}
+
+	return nil
+}
+
+func (u *articleUsecase) Update(inputDetail article.UpdateRequest, inputImage *multipart.FileHeader) error {
+
+	articleData, err := u.articleRepo.GetById(inputDetail.ID)
+
+	if err != nil {
+		return article.ErrArticleNotFound
+	}
+
+	articleUpdate := entity.Article{
+		Title:       inputDetail.Title,
+		Topic:       constant.TOPICS[inputDetail.Topic],
+		Author:      inputDetail.Author,
+		Description: inputDetail.Description,
+	}
+
+	if inputImage != nil {
+		err := u.image.DeleteImageFromS3(articleData.Image)
+
+		if err != nil {
+			return article.ErrInternalServerError
+		}
+
+		path, err := u.image.UploadImageToS3(inputImage)
+
+		if err != nil {
+			return article.ErrInternalServerError
+		}
+
+		articleUpdate.Image = path
+
+	}
+
+	err = u.articleRepo.Update(articleData.ID, articleUpdate)
+
+	if err != nil {
+		return article.ErrInternalServerError
+	}
+
+	return nil
+}
+
+func (u *articleUsecase) Delete(id string) error {
+
+	articleData, err := u.articleRepo.GetById(id)
+
+	if err != nil {
+		return article.ErrArticleNotFound
+	}
+
+	err = u.articleRepo.Delete(articleData.ID)
+
+	if err != nil {
+		return article.ErrInternalServerError
+	}
+
+	return nil
+}
