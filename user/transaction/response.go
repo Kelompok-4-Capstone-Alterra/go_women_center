449f5cea3e6878cdbde2e3a2e22ceab659b00dab
ThiccPan <ihilmiderian@gmail.com>
2023-06-15 03:51:48 +0700

    fix: add saved data to response

--- user/transaction/response.go
+++ user/transaction/response.go
@@ -2,5 +2,6 @@ package transaction
 
 type SendTransactionResponse struct {
 	TransactionID string `json:"transaction_id"`
-	PaymentLink string `json:"payment_link"`
+	PaymentLink   string `json:"payment_link"`
+	Data          interface{}
 }
