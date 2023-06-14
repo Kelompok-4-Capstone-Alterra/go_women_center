53508900253854b4b423f08c0d8eddba052b71b8
ThiccPan <ihilmiderian@gmail.com>
2023-06-15 03:51:26 +0700

    feature: add error message

--- user/transaction/error.go
+++ user/transaction/error.go
@@ -5,4 +5,7 @@ import "errors"
 var (
 	ErrorInvalidGenre error = errors.New("invalid genre code")
 	ErrorInsertDB error = errors.New("error inserting new data to db")
+	ErrorInvalidRequest error = errors.New("error invalid callback")
+	ErrorTransactionNotFound error = errors.New("transaction not found in db")
+	ErrorInvalidPaymentStatus error = errors.New("error invalid payment status")
 )
