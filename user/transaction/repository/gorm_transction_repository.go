ae09fd35c3ec4aabbd7afd5e9f1f99bc5f535f76
ThiccPan <ihilmiderian@gmail.com>
2023-06-14 16:43:28 +0700

    feature: initial repository create transaction

---
+++ user/transaction/repository/gorm_transction_repository.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/entity"
+	"gorm.io/gorm"
+)
+
+type MysqlTransactionRepository interface {
+	CreateTransaction(transaction entity.Transaction) (entity.Transaction, error)
+}
+
+type mysqlTransactionRepository struct {
+	DB *gorm.DB
+}
+
+func NewMysqltransactionRepository(db *gorm.DB) MysqlTransactionRepository {
+	return &mysqlTransactionRepository{
+		DB: db,
+	}
+}
+
+func (tr *mysqlTransactionRepository) CreateTransaction(transaction entity.Transaction) (entity.Transaction, error) {
+	err := tr.DB.Create(&transaction).Error
+	if err != nil {
+		return entity.Transaction{}, err
+	}
+	return transaction, nil
+}
