1bbee91a20b2014b2e445d3d5921b0f17f0ed325
ThiccPan <ihilmiderian@gmail.com>
2023-06-15 03:52:35 +0700

    feature: getbyid, update status, create w rback

--- user/transaction/repository/gorm_transction_repository.go
+++ user/transaction/repository/gorm_transction_repository.go
@@ -2,12 +2,14 @@ package repository
 
 import (
 	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/entity"
-	trError "github.com/Kelompok-4-Capstone-Alterra/go_women_center/user/transaction"
+	trData "github.com/Kelompok-4-Capstone-Alterra/go_women_center/user/transaction"
 	"gorm.io/gorm"
 )
 
 type MysqlTransactionRepository interface {
 	CreateTransaction(transaction entity.Transaction) (entity.Transaction, error)
+	GetById(id string) (entity.Transaction, error)
+	UpdateStatusByData(savedData entity.Transaction, newStatus string) (entity.Transaction, error)
 }
 
 type mysqlTransactionRepository struct {
@@ -21,9 +23,72 @@ func NewMysqltransactionRepository(db *gorm.DB) MysqlTransactionRepository {
 }
 
 func (tr *mysqlTransactionRepository) CreateTransaction(transaction entity.Transaction) (entity.Transaction, error) {
+	// TODO: implement transaction
 	err := tr.DB.Create(&transaction).Error
 	if err != nil {
-		return entity.Transaction{}, trError.ErrorInsertDB
+		return entity.Transaction{}, trData.ErrorInsertDB
 	}
 	return transaction, nil
+}
+
+func (tr *mysqlTransactionRepository) GetById(id string) (entity.Transaction, error) {
+	savedTransaction := entity.Transaction{}
+	err := tr.DB.Where("id = ?", id).First(&savedTransaction).Error
+	if err != nil {
+		return entity.Transaction{}, err
+	}
+
+	return savedTransaction, nil
+}
+
+func (tr *mysqlTransactionRepository) UpdateStatusByData(savedData entity.Transaction, newStatus string) (entity.Transaction, error) {
+	savedData.Status = trData.Status(newStatus)
+	err := tr.DB.Updates(&savedData).Error
+	if err != nil {
+		return entity.Transaction{}, err
+	}
+
+	return savedData, nil
+}
+/*
+create transaction db data, return db instance with transaction 
+
+call CommitCreate if payment success
+
+call RollbackCreate if payment failed
+*/
+func (tr *mysqlTransactionRepository) StartCreate(transaction entity.Transaction) (*gorm.DB, error) {
+	tx := tr.DB.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	err := tx.Error
+	if err != nil {
+		return nil, err
+	}
+
+	err = tx.Create(&transaction).Error
+	if err != nil {
+		return nil, trData.ErrorInsertDB
+	}
+	return tx, nil
+}
+
+// cancel creating transaction data
+// if sending payment req to payment gateway failed
+func (tr *mysqlTransactionRepository) RollbackCreate(tx *gorm.DB) {
+	tx.Rollback()
+}
+
+// commit creating transaction data
+// if sending payment req to payment gateway success
+func (tr *mysqlTransactionRepository) CommitCreate(tx *gorm.DB) error {
+	err := tx.Commit().Error
+	if err != nil {
+		return trData.ErrorInsertDB
+	}
+	return nil
 }
