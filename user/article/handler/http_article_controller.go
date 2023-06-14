eee1ae5ea32c0d721e79ea46b9366fd30e8c5509
Saident <averilprimayuda@gmail.com>
2023-06-14 22:24:44 +0700

    fix response code

--- user/article/handler/http_article_controller.go
+++ user/article/handler/http_article_controller.go
@@ -154,10 +154,10 @@ func (h *articleHandler) DeleteComment(c echo.Context) error {
 	err := h.ArticleUsecase.DeleteComment(commentReq.UserID, commentReq.ArticleID, commentReq.CommentID)
 
 	if err != nil {
-		status := http.StatusInternalServerError
+		status := http.StatusNotFound
 
 		switch err {
-		case article.ErrArticleNotFound:
+		case article.ErrCommentNotFound:
 			status = http.StatusNotFound
 		}
 
