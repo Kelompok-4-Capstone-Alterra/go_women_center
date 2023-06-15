745a6ce373ab1c41eff7662aabd4654e55e40faa
ThiccPan <ihilmiderian@gmail.com>
2023-06-15 23:31:43 +0700

    feature: add more validation method

--- user/transaction/error.go
+++ user/transaction/error.go
@@ -3,10 +3,13 @@ package transaction
 import "errors"
 
 var (
-	ErrorInvalidGenre error = errors.New("invalid genre code")
-	ErrorInsertDB error = errors.New("error inserting new data to db")
-	ErrorInvalidRequest error = errors.New("error invalid callback")
-	ErrorTransactionNotFound error = errors.New("transaction not found in db")
-	ErrorInvalidPaymentStatus error = errors.New("error invalid payment status")
-	ErrorMidtrans error = errors.New("error when sending payment to midtrans")
+	ErrorInvalidGenre            error = errors.New("invalid genre code")
+	ErrorInsertDB                error = errors.New("error inserting new data to db")
+	ErrorInvalidRequest          error = errors.New("error invalid callback")
+	ErrorTransactionNotFound     error = errors.New("transaction not found in db")
+	ErrorInvalidPaymentStatus    error = errors.New("error invalid payment status")
+	ErrorMidtrans                error = errors.New("error when sending payment to midtrans")
+	ErrRequired                  error = errors.New("all fields are required")
+	ErrInvalidUUID               error = errors.New("invalid uuid format")
+	ErrInvalidConsultationMethod error = errors.New("invalid consultation method")
 )
--- user/transaction/handler/validation.go
+++ user/transaction/handler/validation.go
@@ -1,1 +1,39 @@
 package handler
+
+import (
+	"strings"
+
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/constant"
+	"github.com/Kelompok-4-Capstone-Alterra/go_women_center/user/transaction"
+	"github.com/go-playground/validator"
+)
+
+func isRequestValid(m interface{}) error {
+	
+	validate := validator.New()
+	err := validate.Struct(m)
+
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			field := strings.ToLower(err.Field())
+
+			if err.Tag() == "required" {
+				return transaction.ErrRequired
+			}
+
+			switch field {
+			case "counselor_id":
+				return transaction.ErrInvalidUUID
+		}
+		}
+	}
+
+	return nil
+}
+
+func isValidTopic(req transaction.SendTransactionRequest) error {
+	if !constant.METHOD[req.ConsultationMethod] {
+		return transaction.ErrInvalidConsultationMethod
+	}
+	return nil
+}
