795ce429dfdff7c469f8e52b93f6fee6aa53245e
ThiccPan <ihilmiderian@gmail.com>
2023-06-17 04:17:44 +0700

    chore: add new request type

--- admin/transaction/request.go
+++ admin/transaction/request.go
@@ -4,3 +4,7 @@ type SendLinkRequest struct {
 	TransactionId string `json:"transaction_id" validate:"required,uuid"`
 	Link          string `json:"link" validate:"required"`
 }
+
+type CancelTransactionRequest struct {
+	TransactionId string `json:"transaction_id" validate:"required,uuid"`
+}
