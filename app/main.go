1c4d188a2667797d41e3a71ac5f2f8b737796d1c
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2023-06-19 13:46:04 +0700

    command ssl

--- app/main.go
+++ app/main.go
@@ -103,10 +103,10 @@ func main() {
 		DB_Name:     os.Getenv("DB_NAME"),
 	}
 
-	sslconf := config.SSLconf{
-		SSL_CERT:        os.Getenv("SSL_CERT"),
-		SSL_PRIVATE_KEY: os.Getenv("SSL_PRIVATE_KEY"),
-	}
+	// sslconf := config.SSLconf{
+	// 	SSL_CERT:        os.Getenv("SSL_CERT"),
+	// 	SSL_PRIVATE_KEY: os.Getenv("SSL_PRIVATE_KEY"),
+	// }
 
 	googleOauthConfig := &oauth2.Config{
 		RedirectURL:  "http://localhost:8080/google/callback",
@@ -125,7 +125,7 @@ func main() {
 	)
 
 	db := dbconf.InitDB()
-	sslconf.InitSSL()
+	// sslconf.InitSSL()
 
 	// helper
 	jwtConf := helper.NewAuthJWT(os.Getenv("JWT_SECRET_USER"), os.Getenv("JWT_SECRET_ADMIN"))
@@ -260,7 +260,6 @@ func main() {
 		users.GET("/public/articles/:id", userArticleHandler.GetById)
 	}
 
-	
 	restrictUsers := e.Group("/users", userAuthMidd.JWTUser(), userAuthMidd.CheckUser(userAuthUsecase))
 	{
 		restrictUsers.GET("/profile", userHandler.GetById)
@@ -288,7 +287,7 @@ func main() {
 
 		restrictUsers.POST("/reading-lists/save", ReadingListArticleH.Create)
 		restrictUsers.DELETE("/reading-lists/save/:id", ReadingListArticleH.Delete)
-	
+
 		restrictUsers.GET("/articles", userArticleHandler.GetAll)
 		restrictUsers.GET("/articles/:id", userArticleHandler.GetById)
 		restrictUsers.POST("/articles/:id/comments", userArticleHandler.CreateComment)
@@ -341,8 +340,8 @@ func main() {
 	}
 
 	// ssl
-	e.Logger.Fatal(e.StartTLS(":8080", "./ssl/certificate.crt", "./ssl/private.key"))
+	// e.Logger.Fatal(e.StartTLS(":8080", "./ssl/certificate.crt", "./ssl/private.key"))
 
-	// e.Logger.Fatal(e.Start(":8080"))
-	
+	e.Logger.Fatal(e.Start(":8080"))
+
 }
