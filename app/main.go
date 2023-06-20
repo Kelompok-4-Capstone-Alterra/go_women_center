33fc3d1b6384f66a805e0a8378bcfe85a2417f98
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2023-06-20 20:01:24 +0700

    uncommand ssl

--- app/main.go
+++ app/main.go
@@ -104,10 +104,10 @@ func main() {
 		DB_Name:     os.Getenv("DB_NAME"),
 	}
 
-	// sslconf := config.SSLconf{
-	// 	SSL_CERT:        os.Getenv("SSL_CERT"),
-	// 	SSL_PRIVATE_KEY: os.Getenv("SSL_PRIVATE_KEY"),
-	// }
+	sslconf := config.SSLconf{
+		SSL_CERT:        os.Getenv("SSL_CERT"),
+		SSL_PRIVATE_KEY: os.Getenv("SSL_PRIVATE_KEY"),
+	}
 
 	googleOauthConfig := &oauth2.Config{
 		RedirectURL:  "http://localhost:8080/google/callback",
@@ -126,7 +126,7 @@ func main() {
 	)
 
 	db := dbconf.InitDB()
-	// sslconf.InitSSL()
+	sslconf.InitSSL()
 
 	// helper
 	jwtConf := helper.NewAuthJWT(os.Getenv("JWT_SECRET_USER"), os.Getenv("JWT_SECRET_ADMIN"))
@@ -346,8 +346,8 @@ func main() {
 	}
 
 	// ssl
-	// e.Logger.Fatal(e.StartTLS(":8080", "./ssl/certificate.crt", "./ssl/private.key"))
+	e.Logger.Fatal(e.StartTLS(":8080", "./ssl/certificate.crt", "./ssl/private.key"))
 
-	e.Logger.Fatal(e.Start(":8080"))
+	// e.Logger.Fatal(e.Start(":8080"))
 
 }
