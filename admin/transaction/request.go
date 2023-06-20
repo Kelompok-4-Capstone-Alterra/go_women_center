e9673e7699f6469ba41006f4374b5edfeddfc192
ThiccPan <ihilmiderian@gmail.com>
2023-06-20 23:24:18 +0700

    feature: report request struct

--- admin/transaction/request.go
+++ admin/transaction/request.go
@@ -15,3 +15,14 @@ type SendLinkRequest struct {
 type CancelTransactionRequest struct {
 	TransactionId string `json:"transaction_id" validate:"required,uuid"`
 }
+
+type ReportRequest struct {
+	StartDate  string    `query:"start_date" validate:"omitempty"`
+	EndDate    string    `query:"end_date" validate:"omitempty"`
+	Page       int    `query:"page" validate:"omitempty"`
+	Limit      int    `query:"limit" validate:"omitempty"`
+	Search     string `query:"search"`
+	SortBy     string `query:"sort_by"`
+	Offset     int
+	IsDownload bool
+}
