1d1e53263114a21034acb0c69147348e82d2647b
Saident <averilprimayuda@gmail.com>
2023-06-21 16:06:49 +0700

    fix request

--- admin/career/request.go
+++ admin/career/request.go
@@ -3,28 +3,39 @@ package career
 import "mime/multipart"
 
 type CreateRequest struct {
-	JobPosition    string                `form:"job_position" validate:"required"`
-	CompanyName    string                `form:"company_name" validate:"required"`
-	Location       string                `form:"location" validate:"required"`
-	Salary         float64               `form:"salary" validate:"required"`
-	MinExperience  string                `form:"min_experience" validate:"required"`
-	LastEducation  string                `form:"last_education" validate:"required"`
-	Description    string                `form:"description" validate:"required"`
-	CompanyEmail   string                `form:"company_email" validate:"required,email"`
-	Image *multipart.FileHeader 		 `form:"image" validate:"required"`
+	JobPosition   string                `form:"job_position" validate:"required"`
+	CompanyName   string                `form:"company_name" validate:"required"`
+	Location      string                `form:"location" validate:"required"`
+	MinSalary     float64               `form:"min_salary" validate:"omitempty"`
+	MaxSalary     float64               `form:"max_salary" validate:"omitempty"`
+	MinExperience string                `form:"min_experience" validate:"required"`
+	LastEducation string                `form:"last_education" validate:"required"`
+	Description   string                `form:"description" validate:"required"`
+	Requirement   string                `form:"requirement" validate:"required"`
+	CompanyEmail  string                `form:"company_email" validate:"required,email"`
+	Image         *multipart.FileHeader `form:"image" validate:"required"`
 }
 
 type UpdateRequest struct {
-	ID            string  `param:"id" validate:"required,uuid"`
-	JobPosition   string  `form:"job_position" validate:"omitempty"`
-	CompanyName   string  `form:"company_name" validate:"omitempty"`
-	Location      string  `form:"location" validate:"omitempty"`
-	Salary        float64 `form:"salary" validate:"omitempty,number"`
-	MinExperience string  `form:"min_experience" validate:"omitempty"`
-	LastEducation string  `form:"last_education" validate:"omitempty"`
-	Description   string  `form:"description" validate:"omitempty"`
-	CompanyEmail  string  `form:"company_email" validate:"omitempty,email"`
-	Image *multipart.FileHeader `form:"image" validate:"omitempty,required"`
+	ID            string                `param:"id" validate:"required,uuid"`
+	JobPosition   string                `form:"job_position" validate:"omitempty"`
+	CompanyName   string                `form:"company_name" validate:"omitempty"`
+	Location      string                `form:"location" validate:"omitempty"`
+	MinSalary     float64               `form:"min_salary" validate:"omitempty,number"`
+	MaxSalary     float64               `form:"max_salary" validate:"omitempty,number"`
+	MinExperience string                `form:"min_experience" validate:"omitempty"`
+	LastEducation string                `form:"last_education" validate:"omitempty"`
+	Description   string                `form:"description" validate:"omitempty"`
+	Requirement   string                `form:"requirement" validate:"omitempty"`
+	CompanyEmail  string                `form:"company_email" validate:"omitempty,email"`
+	Image         *multipart.FileHeader `form:"image" validate:"omitempty,required"`
+}
+
+type GetAllRequest struct {
+	Page   int    `query:"page"`
+	Limit  int    `query:"limit"`
+	Search string `query:"search"`
+	SortBy string `query:"sort_by" validate:"omitempty,oneof=newest highest_salary"`
 }
 
 type IdRequest struct {
