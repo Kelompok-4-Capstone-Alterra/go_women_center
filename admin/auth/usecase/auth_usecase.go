af58d1645e3105b295bad0caa911d940ab5df20f
Ganes556 <gustiganes@gmail.com>
2023-06-22 23:27:36 +0800

    fix: validation

--- admin/auth/usecase/auth_usecase.go
+++ admin/auth/usecase/auth_usecase.go
@@ -26,6 +26,9 @@ func NewAuthUsecase(repo repository.AdminRepo, encryptor helper.Encryptor) AuthU
 func (u *authUseCase) Login(request auth.LoginAdminRequest) (entity.Admin, error) {
 	data, err := u.Repo.GetByUsername(request.Username)
 	if err != nil {
+		if err.Error() == "record not found" {
+			return entity.Admin{}, auth.ErrInvalidCredential
+		}
 		return entity.Admin{}, auth.ErrInternalServerError
 	}
 	if !u.Encryptor.CheckPasswordHash(request.Password, data.Password) {
